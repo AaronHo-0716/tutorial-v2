7295b017a53279ac001d169d7c1b8704a8b06669
yanxun3737 <hoyanxun@gmail.com>
2021-03-13 22:23:15 +0800

    init

--- App.js
+++ App.js
@@ -1,15 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, Touchable } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
+const Stack = createStackNavigator()
+
+function Homescreen({navigation}){
+  return(
+    <View style={styles.container}>
+      <Text>This is the Homescreen</Text>
+      <Text style={styles.textStyles}>Click Amelia to go to the next screen</Text>
+      <TouchableOpacity onPress={() => {
+        navigation.navigate('Second')
+      }}>
+        <Image source={require('./assets/amelia.jpg')} />
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+function Secondscreen({navigation}){
+  return(
+    <View style={styles.container}>
+      <Text>This is the Secondscreen</Text>
+      <Button
+      title='Go to third screen'
+      onPress={() => {
+        navigation.navigate('Third')
+      }}
+      />
+    </View>
+  )
+}
+
+function Thirdscreen({navigation}){
+  return(
+    <View style={styles.container}>
+      <Text>This is the Thirdscreen</Text>
+      <Button
+      title='Go back homescreen'
+      onPress={() => {
+        navigation.navigate('Home')
+      }}
+      />
+    </View>
+  )
+}
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+        name='Home'
+        component={Homescreen}
+        />
+        <Stack.Screen
+        name='Second'
+        component={Secondscreen}
+        />
+        <Stack.Screen
+        name='Third'
+        component={Thirdscreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -18,4 +76,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyles:{
+    color: '#f5f064',
+    fontSize: 20
+  }
 });
